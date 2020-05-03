bf521441b9a6daff061e2f4f5cbbd47a8def69ab
Vincent Manoukian <10980775+manoukianv@users.noreply.github.com>
2020-05-04 00:11:31 +0200

    Try to stabilise

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,9 @@ button btn4 = { .pin = 18 };
 button btn5 = { .pin = 5 };
 button btn6 = { .pin = 17 };
 button btn7 = { .pin = 16 };
-button* btn[] = { &btn0, &btn1, &btn2, &btn3, &btn4, &btn5, &btn6, &btn7 };
+button btn8 = { .pin = 4 };
+button btn9 = { .pin = 0 };
+button* btn[] = { &btn0, &btn1, &btn2, &btn3, &btn4, &btn5, &btn6, &btn7, &btn8, &btn9 };
 const int N_BUTTONS = sizeof(btn) / sizeof(btn[0]);
 
 joystick axisX = { .pin = 34, .min = -445, .max = 495 };
@@ -61,7 +63,7 @@ void loop() {
   }
   applyDeadzone(axis[0], axis[1], 20);
 
-  bool stateChange;
+  bool stateChange = false;
 
   for (int i = 0; i < N_BUTTONS; i++) {
     if (btn[i]->state != btn[i]->prevState) {
@@ -84,11 +86,18 @@ void loop() {
     int8_t x = axis[0]->state;
     int8_t y = axis[1]->state;
     int8_t z = axis[2]->state;
-    uint8_t b = 0;
+    uint8_t b0 = 0, b1 = 0;
     for (int i = 0; i < N_BUTTONS; i++) {
-      b |= btn[i]->state << i;
+      int index =  i / 8;
+      int bitpos = i % 8;
+      if (index == 0) { 
+        b0 |= btn[i]->state << bitpos;
+      } else {
+        b1 |= btn[i]->state << bitpos;
+      }
+      //Serial.printf("%d-b[%d] << %d\n", i, index, bitpos);
     }
-    uint8_t a[] = {x, y, z, b, b};
+    uint8_t a[] = {(uint8_t)x, (uint8_t)y, (uint8_t)z, b0, b1};
     input->setValue(a, sizeof(a));
     input->notify();
   }
